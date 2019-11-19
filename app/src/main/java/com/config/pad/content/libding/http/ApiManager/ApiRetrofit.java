2c829aba39a2c469088c5f829e4ed0766692af2b
涂双喜 <759110245@qq.com>
2019-11-19 11:31:11 +0800

    StuR2Mvp

--- app/src/main/java/com/config/pad/content/libding/http/ApiManager/ApiRetrofit.java
+++ app/src/main/java/com/config/pad/content/libding/http/ApiManager/ApiRetrofit.java
@@ -5,6 +5,8 @@ import com.config.pad.content.libding.http.ApiService;
 import com.config.pad.content.libding.utils.NetUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
@@ -13,6 +15,7 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -94,4 +97,32 @@ public class ApiRetrofit {
                     .build();
         }
     };
+
+
+    /**
+
+     * Function：打印请求参数和返回结果
+     */
+    public class LoggingInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Interceptor.Chain chain) throws IOException {
+            //这个chain里面包含了request和response，所以你要什么都可以从这里拿
+            Request request = chain.request();
+
+            long t1 = System.nanoTime();//请求发起的时间
+//        com.orhanobut.logger.Logger.e(String.format("发送请求 %s %n%s",
+//                request.url(),  chain.request().body().toString()));
+
+            com.orhanobut.logger.Logger.e(String.format("发送请求 %s %n%s", request.url(),  chain.request().body()==null?"NONE":chain.request().body().toString()));
+            Response response = chain.proceed(request);
+            long t2 = System.nanoTime();//收到响应的时间
+
+            //这里不能直接使用response.body().string()的方式输出日志
+            //因为response.body().string()之后，response中的流会被关闭，程序会报错，我们需要创建出一
+            //个新的response给应用层处理
+            ResponseBody responseBody = response.peekBody(1024 * 1024);
+            com.orhanobut.logger.Logger.e(String.format(Locale.getDefault(), "接收响应: [%s] %n返回json:【%s】 %.1fms", response.request().url(), responseBody.string(), (t2 - t1) / 1e6d));
+            return response;
+        }
+    }
 }
